9fce46488f208d744817daac2b5be996f3b6d271
Cody Doerr (Pian) <40671276+codydoerr@users.noreply.github.com>
2022-01-18 09:24:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CTK302-Template
---
+++ Unit1/hello/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+  textAlign(CENTER) ;
+}
+
+function draw() {
+  background('blue') ;
+  fill('white') ;
+  textSize(32) ;
+  text("hello world!", 250, 250) ;
+}
