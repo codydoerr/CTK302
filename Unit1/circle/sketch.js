c4f4dfce637524e7f8feef3bbdf3cc64e221248c
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-02-05 11:20:38 -0600

    circles finished

---
+++ Unit1/circle/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(500, 500);
+  print("hello world");
+}
+
+function draw() {
+  fill(255,0,0);
+  circle(width/2,height/2,200);
+}
---
+++ Unit1/circles/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  fill(255,0,0);
+  circle(mouseX,mouseY,10);
+}
