64c54d3627ea5d0ef8074db8c5aa60da1fdea0d8
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-02-06 11:00:42 -0600

    font changes

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,27 @@
-function setup() {
-  createCanvas(500, 500);
+let clearFont;
+let roughFont;
+function preload(){
+  clearFont = loadFont("assets/race_sport/Race Sport Free.ttf");
+  roughFont = loadFont("assets/road_rage/Road_Rage.otf");
 }
+function setup() {
+  createCanvas(1000, 1000);
+  rectMode(CENTER);
+  push();
+    fill("red");
+    textFont(clearFont);
+    textSize(36);
+    text("RACE DAY SATURDAY NIGHT!",width/2,height/2-20,1000,36);
+  pop();
+  push();
+    fill("red");
+    textFont(roughFont);
+    textSize(32);
+    text("SHOWDOWN FOR THE CHAMPIONSHIP",width/2,height/2+20
+    ,width,5);
+  pop();
 
+}
 function draw() {
 
 }
