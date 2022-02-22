29edb64047bfd448b1608349927e7a32fdfc7be7
codydoerr <cody.doerr@yahoo.com>
2022-02-22 10:10:14 -0600

    smol change

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -2,39 +2,48 @@ let song1;
 let song2;
 let song3;
 let songState = 0;
-function preload(){
+
+function preload() {
     song1 = loadSound('assets\ellary-loop-1.mp3');
     song2 = loadSound('assets\ellary-loop-2.mp3');
     song3 = loadSound('assets\ellary-loop-3.mp3');
 }
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 }
+
 function draw() {
-    switch(songState){
+    switch (songState) {
         case 0:
-        song1.play();
-        songState++;
-        break;
+            song2.pause();
+            song1.play();
+            songState++;
+            break;
         case 1:
-        background('black');
-        break;
+            background('black');
+            break;
         case 2:
-        song2.play();
-        songState++;
-        break;
+            song1.pause();
+            song2.play();
+            songState++;
+            break;
         case 3:
-        break;
+            background('black');
+            break;
         case 4:
-        song3.play();
-        songState++;
-        break;
+            song2.pause();
+            song3.play();
+            songState++;
+            break;
         case 5:
-        break;
+            background('black');
+            break;
     }
 
 }
-function mouseReleased(){
+
+function mouseReleased() {
     songState++;
     songState = songState % 6
 }
