d748ca12155570a6f328bcfd449bd0fbbdf7cd97
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-04-28 00:04:33 -0500

    draws cool lines

--- Unit4/gecholocation/sketch.js
+++ Unit4/gecholocation/sketch.js
@@ -23,122 +23,122 @@ const mappa = new Mappa('Leaflet');
 // Lets put all our map options in a single object
 const options = {
   //Shows Peoria, Morton, and Bloomington
-    lat: 40.5428,
-    lng: -89.2593,
-    zoom: 11,
-    style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+  lat: 40.5428,
+  lng: -89.2593,
+  zoom: 11,
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
-function preload() {
-    font1 = loadFont("assets/Anton.ttf");
-}
-function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    num = 0;
-    flower = loadImage("assets/Spring.png");
-    textFont(font1, 24);
-    //setInterval(updateLocations,30000);
-    // intervalCurrentPosition(positionPing, 5000); // this is what calls positionPing function
-    myMap = mappa.tileMap(options);
-    myMap.overlay(canvas);
-    imageMode(CENTER);
-    textAlign(CENTER);
 
-    locationData = getCurrentPosition();
+function preload() {
+  font1 = loadFont("assets/Anton.ttf");
 }
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  num = 0;
+  flower = loadImage("assets/Spring.png");
+  textFont(font1, 24);
+  //setInterval(updateLocations,30000);
+  // intervalCurrentPosition(positionPing, 5000); // this is what calls positionPing function
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+  imageMode(CENTER);
+  textAlign(CENTER);
+
+  locationData = getCurrentPosition();
+}
+
 function draw() {
-    clear();
-    if (name == null) {
-      name = prompt("What is your name?");
-      myColor = prompt("What is your favorite color?");
-    }
-    if(washington == null){
-        washington = new EchoMapPin(myMap.latLngToPixel(40.7036,-89.4073),null);
-    }
-    else {
-        washington = new EchoMapPin(myMap.latLngToPixel(40.7036,-89.4073),washington.neighbor);
-    }
-    if(peoria == null){
-        peoria = new EchoMapPin(myMap.latLngToPixel(40.6936, -89.5890),null);
-    }
-    else{
-        peoria = new EchoMapPin(myMap.latLngToPixel(40.6936, -89.5890),peoria.neighbor);
-    }
-    if(morton == null){
-        morton = new EchoMapPin(myMap.latLngToPixel(40.6128, -89.4593),null);
-    }
-    else{
-        morton = new EchoMapPin(myMap.latLngToPixel(40.6128, -89.4593),morton.neighbor);
-    }
-    if(bloomington == null){
-        bloomington = new EchoMapPin(myMap.latLngToPixel(40.4842, -88.9937),null);
-    }
-    else {
-        bloomington = new EchoMapPin(myMap.latLngToPixel(40.4842, -88.9937),bloomington.neighbor);
-    }
-    if(pekin == null){
-        pekin = new EchoMapPin(myMap.latLngToPixel(40.5675,-89.6407),null);
-    }
-    else {
-        pekin = new EchoMapPin(myMap.latLngToPixel(40.5675,-89.6407),pekin.neighbor);
-    }
-    if(normal == null){
-        normal = new EchoMapPin(myMap.latLngToPixel(40.5142,-88.9906),null);
-    }
-    else {
-        normal = new EchoMapPin(myMap.latLngToPixel(40.5142,-88.9906),normal.neighbor);
-    }
-    if(myPin == null){
-      myPin = new EchoMapPin(myMap.latLngToPixel(locationData.latitude,locationData.longitude),null);
-    }
-    else {
-      myPin = new EchoMapPin(myMap.latLngToPixel(locationData.latitude,locationData.longitude),myPin.neighbor);
-    }
-    peoria.display();
-    morton.display();
-    bloomington.display();
-    washington.display();
-    pekin.display();
-    normal.display();
-    if(myPin != null){
-      myPin.display();
-    }
+  clear();
+  if (name == null) {
+    name = prompt("What is your name?");
+    myColor = prompt("What is your favorite color?");
+  }
+  if (washington == null) {
+    washington = new EchoMapPin(myMap.latLngToPixel(40.7036, -89.4073), null);
+  } else {
+    washington = new EchoMapPin(myMap.latLngToPixel(40.7036, -89.4073), washington.neighbor);
+  }
+  if (peoria == null) {
+    peoria = new EchoMapPin(myMap.latLngToPixel(40.6936, -89.5890), null);
+  } else {
+    peoria = new EchoMapPin(myMap.latLngToPixel(40.6936, -89.5890), peoria.neighbor);
+  }
+  if (morton == null) {
+    morton = new EchoMapPin(myMap.latLngToPixel(40.6128, -89.4593), null);
+  } else {
+    morton = new EchoMapPin(myMap.latLngToPixel(40.6128, -89.4593), morton.neighbor);
+  }
+  if (bloomington == null) {
+    bloomington = new EchoMapPin(myMap.latLngToPixel(40.4842, -88.9937), null);
+  } else {
+    bloomington = new EchoMapPin(myMap.latLngToPixel(40.4842, -88.9937), bloomington.neighbor);
+  }
+  if (pekin == null) {
+    pekin = new EchoMapPin(myMap.latLngToPixel(40.5675, -89.6407), null);
+  } else {
+    pekin = new EchoMapPin(myMap.latLngToPixel(40.5675, -89.6407), pekin.neighbor);
+  }
+  if (normal == null) {
+    normal = new EchoMapPin(myMap.latLngToPixel(40.5142, -88.9906), null);
+  } else {
+    normal = new EchoMapPin(myMap.latLngToPixel(40.5142, -88.9906), normal.neighbor);
+  }
+  if (myPin == null) {
+    myPin = new EchoMapPin(myMap.latLngToPixel(locationData.latitude, locationData.longitude), null);
+  } else {
+    myPin = new EchoMapPin(myMap.latLngToPixel(locationData.latitude, locationData.longitude), myPin.neighbor);
+  }
+
+  noFill();
+  beginShape();
+  peoria.display();
+  morton.display();
+  bloomington.display();
+  washington.display();
+  pekin.display();
+  normal.display();
+  if (myPin != null) {
+    myPin.display();
+  }
+  endShape();
 }
+
 function gotData(data) {
-    console.log(data); // Print the data in the console
+  console.log(data); // Print the data in the console
 }
+
 function beginEcho() {
-    locations = [];
-    print(myPin);
-    locations.push(myPin);
-    locations.push(peoria);
-    locations.push(morton);
-    locations.push(bloomington);
-    locations.push(normal);
-    locations.push(washington);
-    locations.push(pekin);
-    let tempDistance;
-    let closestNeighbor;
-    for(let i = 0;i<locations.length;i++){
-      tempDistance = null;
-      for(let j = 0;j<locations.length;j++){
-        if(i==j /*|| locations[j].connected*/){
-          j++;
-        }else if(tempDistance == null){
-          tempDistance = calcGeoDistance(locations[i].pos.x,locations[i].pos.y,locations[j].pos.x,locations[j].pos.y, 'mi');
-          closestNeighbor = locations[j];
-          locations[j].neighbor = locations[i];
-        }
-        else if(calcGeoDistance(locations[i].pos.x,locations[i].pos.y,locations[j].pos.x,locations[j].pos.y, 'mi') < tempDistance){
-          tempDistance = calcGeoDistance(locations[i].pos.x,locations[i].pos.y,locations[j].pos.x,locations[j].pos.y, 'mi');
-          closestNeighbor = locations[j];
-          locations[j].neighbor = locations[i];
-        }
+  locations = [];
+  locations.push(peoria);
+  locations.push(pekin);
+  locations.push(washington);
+  locations.push(morton);
+  locations.push(bloomington);
+  locations.push(normal);
+  locations.push(myPin);
+  let tempDistance;
+  let closestNeighbor;
+  for (let i = 0; i < locations.length; i++) {
+    tempDistance = null;
+    for (let j = 0; j < locations.length; j++) {
+      if (i == j /*|| locations[j].connected*/ ) {
+        j++;
+      } else if (tempDistance == null) {
+        tempDistance = calcGeoDistance(locations[i].pos.x, locations[i].pos.y, locations[j].pos.x, locations[j].pos.y, 'mi');
+        closestNeighbor = locations[j];
+        locations[j].neighbor = locations[i];
+      } else if (calcGeoDistance(locations[i].pos.x, locations[i].pos.y, locations[j].pos.x, locations[j].pos.y, 'mi') < tempDistance) {
+        tempDistance = calcGeoDistance(locations[i].pos.x, locations[i].pos.y, locations[j].pos.x, locations[j].pos.y, 'mi');
+        closestNeighbor = locations[j];
+        locations[j].neighbor = locations[i];
       }
-      locations[i].neighbor = closestNeighbor;
     }
+    locations[i].neighbor = closestNeighbor;
+  }
 }
 class EchoMapPin {
-  constructor(latLong,neighbor) {
+  constructor(latLong, neighbor) {
     this.pos = latLong;
     this.connected = false;
     this.neighbor = neighbor;
@@ -147,8 +147,9 @@ class EchoMapPin {
     this.b = random(255);
   }
   display() {
-    if(this.neighbor != null){
-        line(this.pos.x,this.pos.y,this.neighbor.pos.x,this.neighbor.pos.y);
+    if (this.neighbor != null) {
+      //line(this.pos.x,this.pos.y,this.neighbor.pos.x,this.neighbor.pos.y);
+      vertex(this.pos.x, this.pos.y);
     }
     push();
     fill(myColor);
